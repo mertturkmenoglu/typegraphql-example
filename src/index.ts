522b21bfb28c739b513d1b0b5fe7ac8e0a04c195
Mert <mertturkmenoglu99@gmail.com>
2021-03-31 20:11:10 +0300

    Add TypeORM configs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import 'reflect-metadata';
 
+import path from 'path';
+
 import express from 'express';
+import dotenv from 'dotenv-safe';
+import { createConnection } from 'typeorm';
+
+dotenv.config();
 
 const main = async () => {
+  const conn = await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: true,
+    migrations: [path.join(__dirname, 'migrations')],
+    entities: [],
+  });
+
+  await conn.runMigrations();
+
   const app = express();
 
   app.listen(4000, () => {
